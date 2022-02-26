4a1491f8c3993615cf4db4a6f7882455ee8ed495
Arjun Singh <arjunkhakh@gmail.com>
2022-02-26 16:53:08 +0000

    creating code for the licence functions

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const fs = require("fs")
 const inq = require("inquirer");
 const generateMarkdown = require("./utils/generateMarkdown");
-const gen = require("./utils/generateMarkdown")
+// const gen = require("./utils/generateMarkdown")
 
 // TODO: Create an array of questions for user input
 
@@ -28,9 +28,44 @@ const questions = [
             name: "usage",
         },
         {
-            type: "input",
+            type: "list",
             message: "What is the licence on your application?",
             name: "license",
+            choices: [
+            'Academic Free License v3.0',
+            'Apache license 2.0',
+            'Artistic license 2.0',
+            'Boost Software License 1.0',
+            'BSD 3-clause Clear license',
+            'Creative Commons license family',
+            'Creative Commons Zero v1.0 Universal',
+            'Creative Commons Attribution 4.0',
+            'Creative Commons Attribution Share Alike 4.0',
+            'Do What The F*ck You Want To Public License',
+            'Educational Community License v2.0',
+            'Eclipse Public License 1.0',
+            'Eclipse Public License 2.0',
+            'European Union Public License 1.1',
+            'GNU Affero General Public License v3.0',
+            'GNU General Public License family',
+            'GNU General Public License v2.0',
+            'GNU General Public License v3.0',
+            'GNU Lesser General Public License family',
+            'GNU Lesser General Public License v2.1',
+            'GNU Lesser General Public License v3.0',
+            'ISC',
+            'LaTeX Project Public License v1.3c',
+            'Microsoft Public License',
+            'MIT',
+            'Mozilla Public License 2.0',
+            'Open Software License 3.0',
+            'PostgreSQL License',
+            'SIL Open Font License 1.1',
+            'University of Illinois/NCSA Open Source License',
+            'The Unlicense',
+            'zLib License',
+            'None',
+            ]
         },
         {
             type: "input",
@@ -66,7 +101,7 @@ function writeToFile(filename, data) {
 // TODO: Create a function to initialize app
 function init() {
   inq.prompt(questions).then((answers) => {
-    writeToFile("TestReadME.md", answers);
+    writeToFile("TestReadME14.md", answers);
   }
   )
 }
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,18 +1,34 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license) {
+    return `<img src=https://img.shields.io/badge/License-${license.split(' ').join('_')}-orange.svg>`
+  } else {
+    return " "
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return `[License](#license) `;
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license) {
+    return `This is Licensed by ${license}`
+  } else {
+    return " ";
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${ data.title }
+
+  ${renderLicenseBadge(data.license)}
 
   ## Table of Contents
   
@@ -37,7 +53,7 @@ function generateMarkdown(data) {
   ${ data.usage }
   
   ## License
-  ${ data.license }
+  ${renderLicenseSection(data.license) }
   
   ## Tests
   ${ data.test }
